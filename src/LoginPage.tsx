072d5473d1def6a54b9b3ab4030ffe9b8c0e2282
Yeyun Lu <luyeyun18826077098@outlook.com>
2024-04-29 18:07:44 -0700

    Adjust style and add login logic, todo: implement auth

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -3,6 +3,29 @@ import logo from './asset/logo.png';
 import './asset/login.css';
 
 const LoginPage: React.FC = () => {
+
+    const [userName, setUserName] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const login = () => {
+        console.log(userName)
+        fetch('http://localhost:8080/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                username: userName,
+                password: password
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch(error => {
+                console.log("error")
+            })
+    }
+
     return (
         <div>
             <img src={logo} className="App-logo" alt="logo" />
@@ -13,24 +36,32 @@ const LoginPage: React.FC = () => {
                 </div>
                 <br />
                 <div className="inputContainer">
-                    <input
-                        placeholder="User Name"
-                        className="inputBox"
-                    />
+                    <label className="inputLabel">
+                        UserName:
+                        <input
+                            className="inputBox"
+                            value={userName}
+                            onChange={e => setUserName(e.target.value)}
+                        />
+                    </label>
                     <label className="errorLabel">{ }</label>
                 </div>
                 <br />
                 <div className="inputContainer">
-                    <input
-                        placeholder="Password"
-                        className="inputBox"
-                    />
+                    <label className="inputLabel">
+                        Password:
+                        <input
+                            className="inputBox"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </label>
                     <label className="errorLabel">{ }</label>
                 </div>
                 <br />
                 <br />
                 <div className="buttonContainer">
-                    <button className="login">Login</button>
+                    <button className="login" onClick={() => login()}>Login</button>
                     <button className="signUp">Sign up</button>
                 </div>
             </div>
